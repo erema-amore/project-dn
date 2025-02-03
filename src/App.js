b5a6b980550cff71f48c1855b3d5c84007d3fdcc
KairatJakshylykov <kairat2001jakshylykov@gmail.com>
2025-02-03 18:23:27 +0600

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import TheyTrustUs from './pages/theyTrustUs/TheyTrustUs';
 
 function App() {
   return (
     <div className="App">
-     
+     <TheyTrustUs/>
     </div>
   );
 }
--- src/pages/theyTrustUs/TheyTrustUs.jsx
+++ src/pages/theyTrustUs/TheyTrustUs.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './theyTrustUs.css'
+import photo from '../../images/theyTrustUs/citek.png'
+import photo from '../../images/theyTrustUs/1.png'
+
 
 const TheyTrustUs = () => {
   return (
-    <div>
+    <div className='box'>
+        <h3>Нам доверяют</h3>
+        <div className='img'>
+            <img src={photo} alt="" />
+           
+            
+        </div>
       
     </div>
   )
