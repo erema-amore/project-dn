11a83a01fd9e12057a15c05756d0cbdc0bea97fe
erema-amore <123175538+erema-amore@users.noreply.github.com>
2025-02-04 11:40:04 +0600

    Update App.js
    j

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
-import PopularTires from './pages/popularTires/PopularTires';
+
 
 function App() {
   return (
     <div className="App">
-     <PopularTires/>
+     
      
     </div>
   );
