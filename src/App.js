0b5b3f59f5a3a70d9319812e884f98d7098ca308
erema-amore <123175538+erema-amore@users.noreply.github.com>
2025-02-04 15:00:24 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
-import MainFrame from './pages/mainFrame/MainFrame'
-import Header from './pages/header/Header'
+
 
 
 function App() {
   return (
     <div className="App">
-     <MainFrame/>
-     <Header/>
+     
     </div>
   );
 }
